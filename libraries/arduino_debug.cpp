e2352d21f6f4791b1aa20c33378a7638134fd359
Baptiste Gaultier <b.gaultier@gmail.com>
2012-07-02 15:44:48 +0200

    debug files added

---
+++ libraries/arduino_debug.cpp
@@ -0,0 +1,58 @@
+#include <HardwareSerial.h>
+#include <Arduino.h>
+#include "uip.h"
+#include "arduino_debug.h"
+
+void arduino_debug_init() {
+    #if ARDUINO_DEBUG
+    Serial.begin(9600);
+    #endif
+}
+
+void arduino_debug(char* s) {
+    #if ARDUINO_DEBUG
+    Serial.println(s);
+    delay(20);
+    #endif
+}
+
+void arduino_debug_hex(int b) {
+  #if ARDUINO_DEBUG
+    Serial.println(b, HEX);
+    #endif
+}
+
+void arduino_debug_dec(long b) {
+  #if ARDUINO_DEBUG
+    Serial.println(b, DEC);
+    #endif
+}
+    
+void arduino_debug_address(const uip_ip6addr_t* address) {
+  #if ARDUINO_DEBUG
+    int i;
+    for (i=0; i<16; i++) {    
+      if (address->u8[i]<0xf) 
+        Serial.print("0");   
+      Serial.print(address->u8[i], HEX);
+      if (((i%2) == 1) && (i != 15))
+        Serial.print(":");
+    }    
+    Serial.println();  
+    #endif
+}
+
+void arduino_debug_lladdr(const uip_lladdr_t* address){
+  #if ARDUINO_DEBUG
+    int i;
+    Serial.print(address->addr[0], HEX);
+    for (i=1; i< UIP_LLADDR_LEN; i++) {  
+      Serial.print(":");
+      Serial.print(address->addr[i], HEX);
+    }
+    Serial.println();  
+    #endif
+}
+
+    
+    
---
+++ libraries/arduino_debug.h
@@ -0,0 +1,23 @@
+#ifndef __ARDUINODEBUG_H__
+#define __ARDUINODEBUG_H__
+
+#define ARDUINO_DEBUG 1
+
+#ifdef __cplusplus
+  extern "C" {
+#else
+  #include "uip.h"
+#endif  
+ 
+extern void arduino_debug_init();
+extern void arduino_debug(char* s);
+extern void arduino_debug_hex(int b);
+extern void arduino_debug_dec(long b);
+extern void arduino_debug_address(const uip_ip6addr_t* address);
+extern void arduino_debug_lladdr(const uip_lladdr_t* address);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __ARDUINODEBUG_H__ */
